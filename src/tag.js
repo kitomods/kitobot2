7305c75338b3681ff0926f21d4aff23eea25c038
marcosmods <78054628+marcosmods@users.noreply.github.com>
2021-03-01 13:47:19 -0300

    Add files via upload

---
+++ src/tag.js
@@ -0,0 +1,12 @@
+const tag = (prefix, pushname) => {
+ return ` Tag: '𝐗𝐒
+
+Exemplo: 𝐓𝐄𝐌𝐀𝐑𝐈'𝐗𝐒  -  𝐂𝐄𝐑𝐎𝐋𝐙𝐄𝐑𝐀’𝐗𝐒
+
+Recado: 𝐖𝐞𝐫 𝐛𝐞𝐢𝐦 𝐇𝐚𝐤𝐞𝐧𝐤𝐫𝐞𝐮𝐳 𝐬𝐜𝐡𝐰ö𝐫𝐭, 𝐦𝐮𝐬𝐬 𝐚𝐮𝐟 𝐣𝐞𝐝𝐞 𝐚𝐧𝐝𝐞𝐫𝐞 𝐓𝐫𝐞𝐮𝐞 𝐯𝐞𝐫𝐳𝐢𝐜𝐡𝐭𝐞𝐧
+ `
+
+
+}
+
+exports.tag = tag
